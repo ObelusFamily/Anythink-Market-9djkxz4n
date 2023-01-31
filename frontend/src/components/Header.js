2db7f1d2b6f2f42f63f28d08028d2a4b97de6221
stellaanavi <107404514+stellaanavi@users.noreply.github.com>
2023-01-31 16:35:56 +0000

    translate header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../imgs/topbar_logo.png";
+import { useTranslation } from 'react-i18next';
+
 
 const LoggedOutView = () => {
+  const { t } = useTranslation();
   return (
     <ul className="navbar-nav ml-auto">
       <li className="nav-item">
         <Link to="/login" className="nav-link">
-          Sign in
+        {t('text.signin')}
         </Link>
       </li>
 
       <li className="nav-item">
         <Link to="/register" className="nav-link">
-          Sign up
+        {t('text.signup')}
         </Link>
       </li>
     </ul>
